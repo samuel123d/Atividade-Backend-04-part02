34c662853759121cf946267d81ee7e5e467fb759
Samuel.clipes <121581663+samuel123d@users.noreply.github.com>
2024-10-11 13:38:49 -0300

    Atualizar o produtos.js

--- rotas/produtos.js
+++ rotas/produtos.js
@@ -1,48 +1,51 @@
-const express = require('express')
-const router = express.Router()
-const produtoMid = require('../midware/validar.produto')
-const { Produto } = require('../models')
+const express = require('express');
+const router = express.Router();
+const produtoMid = require('../midware/validar.produto');
+const { Produto } = require('../models');
 
-router.produto('/', produtoMid)
-router.put('/', produtoMid)
+router.use(produtoMid); // Aplica o middleware para todas as rotas
 
 router.get('/', async (req, res) => {
-    const produtos = await produto.findAll()
-    res.json({produtos: produtos})
-})
+    const produtos = await Produto.findAll();
+    res.json({ produtos });
+});
 
 router.get('/:id', async (req, res) => {
-    const produto = await produto.findByPk(req.params.id)
-    res.json({produtos: produto})
-})
+    const produto = await Produto.findByPk(req.params.id);
+    if (produto) {
+        res.json({ produto });
+    } else {
+        res.status(404).json({ msg: "Produto não encontrado!" });
+    }
+});
 
-router.produto('/', async (req, res) => {
-    const produto = await produto.create(req.body)
-    res.json({msg: "produto adicionado com sucesso!"})
-})
+router.post('/', async (req, res) => {
+    const produto = await Produto.create(req.body);
+    res.json({ msg: "Produto adicionado com sucesso!", produto });
+});
 
 router.delete('/', async (req, res) => {
-    const id = req.query.id
-    const produto = await produto.findByPk(id)
-    if (produto){
-        await produto.destroy()
-        res.json({msg: "produto deletado com sucesso!"})
-    }else{
-        res.status(400).json({msg: "produto não encontrado!"})
+    const id = req.query.id;
+    const produto = await Produto.findByPk(id);
+    if (produto) {
+        await produto.destroy();
+        res.json({ msg: "Produto deletado com sucesso!" });
+    } else {
+        res.status(400).json({ msg: "Produto não encontrado!" });
     }
-})
+});
 
 router.put('/', async (req, res) => {
-    const id = req.query.id
-    const produto = await produto.findByPk(id)
-    if (produto){
-        produto.titulo = req.body.titulo
-        produto.texto = req.body.texto
-        await produto.save()
-        res.json({msg: "produto atualizado com sucesso!"})
-    }else{
-        res.status(400).json({msg: "produto não encontrado!"})
+    const id = req.query.id;
+    const produto = await Produto.findByPk(id);
+    if (produto) {
+        produto.titulo = req.body.titulo;
+        produto.texto = req.body.texto;
+        await produto.save();
+        res.json({ msg: "Produto atualizado com sucesso!" });
+    } else {
+        res.status(400).json({ msg: "Produto não encontrado!" });
     }
-})
+});
 
-module.exports = router
+module.exports = router;
